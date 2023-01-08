42801e4c6b62e21e95703140ff4b3e9cb6097d6b
Hazem <hazemzon@gmail.com>
2023-01-08 21:25:19 +0200

    ...

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -12,6 +12,7 @@ export const config = {
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
+  
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
